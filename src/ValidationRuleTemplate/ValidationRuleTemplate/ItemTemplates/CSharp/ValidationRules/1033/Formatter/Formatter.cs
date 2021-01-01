6ca3084e68b0aa0f2cef536153414053d50d905a
luismts <deivi_0326@hotmail.com>
2021-01-01 11:18:23 -0400

    Base project moved

---
+++ src/ValidationRuleTemplate/ValidationRuleTemplate/ItemTemplates/CSharp/ValidationRules/1033/Formatter/Formatter.cs
@@ -0,0 +1,22 @@
+﻿using Plugin.ValidationRules.Interfaces;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$<T> : IValueFormatter<T>
+    {
+
+        public bool Format(T value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            // Your implementation goes here
+
+            
+            return true;
+        }
+    }
+
+}
